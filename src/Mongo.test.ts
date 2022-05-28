f152a75d4a0575b0a17267fc4e1f510fd125ab3c
Andrey <41819110+Vonamugan@users.noreply.github.com>
2022-05-28 16:47:55 +0200

    mongo.createTransaction

--- src/Mongo.test.ts
+++ src/Mongo.test.ts
@@ -1,5 +1,5 @@
 import { MongoClient as MockMongoClient } from 'mongodb';
-import Mongo, { MONGO_URI } from './Mongo';
+import Mongo, { MONGO_URI, DB_NAME, TRANSACTIONS_COLLECTION } from './Mongo';
 
 jest.mock('mongodb');
 
@@ -47,6 +47,98 @@ describe('Mongo', () => {
       // Assert
       expect(mongo.client.connect).toBeCalled();
       expect(mongo.client.close).toBeCalled();
+    });
+  });
+  describe('seed', () => {
+    test('succesfull seed', async () => {
+      // Arrange
+      const mongo = new Mongo();
+      const expectedDbName = DB_NAME;
+      const expectedCollectionName = TRANSACTIONS_COLLECTION;
+      const mockCreateCollection = jest.fn();
+      mongo.client.db = jest
+        .fn()
+        .mockReturnValue({ createCollection: mockCreateCollection });
+
+      // Act
+      await mongo.seed();
+
+      // Assert
+      expect(mongo.client.db).toBeCalledWith(expectedDbName);
+      expect(mockCreateCollection).toBeCalledWith(expectedCollectionName);
+    });
+  });
+  describe('createTransaction', () => {
+    test('happy path', async () => {
+      // Arrange
+      const mongo = new Mongo();
+      const expectedDbName = DB_NAME;
+      const expectedCollectionName = TRANSACTIONS_COLLECTION;
+      const expectedDate = new Date('2022-05-01');
+      const expectedName = 'Пиросмани';
+      const expectedValue = 70;
+      const expectedDocument = { date: expectedDate, name: expectedName, value: expectedValue };
+      const expectedTransactionId = '1';
+      const mockToString = jest
+        .fn()
+        .mockReturnValue(expectedTransactionId);
+      const mockInsertOne = jest.fn(async () => ({
+        insertedId: {
+          toString: mockToString,
+        },
+      }));
+      const mockCollection = jest
+        .fn()
+        .mockReturnValue({ insertOne: mockInsertOne });
+      mongo.client.db = jest
+        .fn()
+        .mockReturnValue({ collection: mockCollection });
+
+      // Act
+      const actualTransactionId = await mongo.createTransaction(
+        expectedDate,
+        expectedName,
+        expectedValue,
+      );
+
+      // Assert
+      expect(mongo.client.db).toBeCalledWith(expectedDbName);
+      expect(mockCollection).toBeCalledWith(expectedCollectionName);
+      expect(mockInsertOne).toBeCalledWith(expectedDocument);
+      expect(actualTransactionId).toEqual(expectedTransactionId);
+    });
+    test('insertOne throws exception', async () => {
+      // Arrange
+      const mongo = new Mongo();
+      const expectedDbName = DB_NAME;
+      const expectedCollectionName = TRANSACTIONS_COLLECTION;
+      const expectedDate = new Date('2022-05-01');
+      const expectedName = 'Пиросмани';
+      const expectedValue = 70;
+      const expectedDocument = { date: expectedDate, name: expectedName, value: expectedValue };
+      const expectedTransactionId = null;
+      const mockInsertOne = jest.fn(async () => {
+        throw new Error('InsertOne exception');
+      });
+      const mockCollection = jest
+        .fn()
+        .mockReturnValue({ insertOne: mockInsertOne });
+      mongo.client.db = jest
+        .fn()
+        .mockReturnValue({ collection: mockCollection });
+
+      // Act
+      const actualTransactionId = await mongo.createTransaction(
+        expectedDate,
+        expectedName,
+        expectedValue,
+      );
+
+      // Assert
+      expect(mongo.client.db).toBeCalledWith(expectedDbName);
+      expect(mockCollection).toBeCalledWith(expectedCollectionName);
+      expect(mockInsertOne).toBeCalledWith(expectedDocument);
+      expect(actualTransactionId).toEqual(expectedTransactionId);
     });
   });
 });
--- src/Mongo.ts
+++ src/Mongo.ts
@@ -1,6 +1,8 @@
 import { MongoClient } from 'mongodb';
 
 export const MONGO_URI = 'mongodb://root:example@localhost:27017/?authMechanism=DEFAULT';
+export const DB_NAME = 'great-equalizer';
+export const TRANSACTIONS_COLLECTION = 'transactions';
 
 export default class Mongo {
   client: MongoClient;
@@ -18,5 +20,28 @@ export default class Mongo {
       console.error(error);
       await this.client.close();
     }
+  }
+
+  async seed() {
+    const database = this.client.db(DB_NAME);
+    try {
+      await database.createCollection(TRANSACTIONS_COLLECTION);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      console.log('db created');
+    }
+  }
+
+  async createTransaction(date: Date, name: string, value: number): Promise<string | null> {
+    const collection = this.client.db(DB_NAME).collection(TRANSACTIONS_COLLECTION);
+    const tranasactionDocument = { date, name, value };
+    try {
+      const result = await collection.insertOne(tranasactionDocument);
+      return result.insertedId.toString();
+    } catch (error) {
+      console.error('insertOne exception');
+    }
+    return null;
   }
 }
